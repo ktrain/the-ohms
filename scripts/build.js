1db955ac1a15b820731d0d6466d2fade085c3d43
Kellen Steffen <kellen.steffen@thalmic.com>
2017-02-11 16:51:07 -0500

    Try a homemade libs function that adds latest Babelify.

--- scripts/build.js
+++ scripts/build.js
@@ -12,7 +12,36 @@ const assets = require('vitreum/steps/assets').partial;
 const project = require('./project.json');
 
 clean()
-	.then(libs(project.libs))
+	.then(() => {
+		const browserify = require('browserify');
+		const babelify = require('babelify');
+		const fs = require('fs');
+		const uglify = require('uglify-js');
+
+		return new Promise((resolve, reject) => {
+			browserify({})
+				.require(project.libs)
+				.transform(babelify, {
+					presets: ['latest'],
+					global: true,
+				})
+				.bundle((err, buf) => {
+					if (err) return reject(err);
+					let code = buf.toString();
+					if (process.env.NODE_ENV === 'production') {
+						try {
+							code = uglify.minify(buf.toString(), { fromString: true }).code;
+						} catch (e) {
+							return reject(e);
+						}
+					}
+					fs.writeFile('build/libs.js', code, (err) => {
+						if (err) return reject(err);
+						return resolve();
+					});
+				});
+		});
+	})
 	.then(jsx('main', './client/main/main.jsx', project.libs, [project.clientDir]))
 	.then(less('main', project.clientDir))
 	.then(assets(project.assets, [project.clientDir]))
