c177fa153f103f6d7c50bdd3a3fc7e4cc9d2f22c
Kellen Steffen <kellen.steffen@thalmic.com>
2017-02-18 23:07:51 -0500

    Move GameSetup data to config.

--- src/data/game.setup.js
+++ src/data/game.setup.js
@@ -1,215 +1,16 @@
 'use strict';
 
 const _ = require('lodash');
+const config = require('nconf');
 
-
-const SetupData = {
-	2: {
-		numSpies: 1,
-		rounds: [
-			{
-				teamSize: 1,
-				numFailsRequired: 1,
-			},
-			{
-				teamSize: 1,
-				numFailsRequired: 1,
-			},
-			{
-				teamSize: 1,
-				numFailsRequired: 1,
-			},
-			{
-				teamSize: 1,
-				numFailsRequired: 1,
-			},
-			{
-				teamSize: 1,
-				numFailsRequired: 1,
-			},
-		],
-	},
-	3: {
-		numSpies: 2,
-		rounds: [
-			{
-				teamSize: 1,
-				numFailsRequired: 1,
-			},
-			{
-				teamSize: 2,
-				numFailsRequired: 2,
-			},
-			{
-				teamSize: 1,
-				numFailsRequired: 1,
-			},
-			{
-				teamSize: 2,
-				numFailsRequired: 2,
-			},
-			{
-				teamSize: 1,
-				numFailsRequired: 1,
-			},
-		],
-	},
-	5: {
-		numSpies: 2,
-		rounds: [
-			{
-				teamSize: 2,
-				numFailsRequired: 1,
-			},
-			{
-				teamSize: 3,
-				numFailsRequired: 1,
-			},
-			{
-				teamSize: 2,
-				numFailsRequired: 1,
-			},
-			{
-				teamSize: 3,
-				numFailsRequired: 1,
-			},
-			{
-				teamSize: 3,
-				numFailsRequired: 1,
-			},
-		],
-	},
-	6: {
-		numSpies: 2,
-		rounds: [
-			{
-				teamSize: 2,
-				numFailsRequired: 1,
-			},
-			{
-				teamSize: 3,
-				numFailsRequired: 1,
-			},
-			{
-				teamSize: 4,
-				numFailsRequired: 1,
-			},
-			{
-				teamSize: 3,
-				numFailsRequired: 1,
-			},
-			{
-				teamSize: 4,
-				numFailsRequired: 1,
-			},
-		],
-	},
-	7: {
-		numSpies: 3,
-		rounds: [
-			{
-				teamSize: 2,
-				numFailsRequired: 1,
-			},
-			{
-				teamSize: 3,
-				numFailsRequired: 1,
-			},
-			{
-				teamSize: 3,
-				numFailsRequired: 1,
-			},
-			{
-				teamSize: 4,
-				numFailsRequired: 2,
-			},
-			{
-				teamSize: 4,
-				numFailsRequired: 1,
-			},
-		],
-	},
-	8: {
-		numSpies: 3,
-		rounds: [
-			{
-				teamSize: 3,
-				numFailsRequired: 1,
-			},
-			{
-				teamSize: 4,
-				numFailsRequired: 1,
-			},
-			{
-				teamSize: 4,
-				numFailsRequired: 1,
-			},
-			{
-				teamSize: 5,
-				numFailsRequired: 2,
-			},
-			{
-				teamSize: 5,
-				numFailsRequired: 1,
-			},
-		],
-	},
-	9: {
-		numSpies: 3,
-		rounds: [
-			{
-				teamSize: 3,
-				numFailsRequired: 1,
-			},
-			{
-				teamSize: 4,
-				numFailsRequired: 1,
-			},
-			{
-				teamSize: 4,
-				numFailsRequired: 1,
-			},
-			{
-				teamSize: 5,
-				numFailsRequired: 2,
-			},
-			{
-				teamSize: 5,
-				numFailsRequired: 1,
-			},
-		],
-	},
-	10: {
-		numSpies: 4,
-		rounds: [
-			{
-				teamSize: 3,
-				numFailsRequired: 1,
-			},
-			{
-				teamSize: 4,
-				numFailsRequired: 1,
-			},
-			{
-				teamSize: 4,
-				numFailsRequired: 1,
-			},
-			{
-				teamSize: 5,
-				numFailsRequired: 2,
-			},
-			{
-				teamSize: 5,
-				numFailsRequired: 1,
-			},
-		],
-	},
-};
+const SetupData = config.get('gameSetup');
 
 
 const GameSetup = {
-	getMinNumPlayers: () => { return 2; },
-	getMaxNumPlayers: () => { return 10; },
+
+	getMinNumPlayers: () => _.min(_.keys(SetupData));
+	getMaxNumPlayers: () => _.max(_.keys(SetupData));
+
 	getGameSetupByNumPlayers: (numPlayers) => {
 		const setup = _.clone(SetupData[numPlayers]);
 		_.each(setup.rounds, (round) => {
@@ -223,6 +24,7 @@ const GameSetup = {
 		});
 		return setup;
 	},
+
 };
 
 module.exports = GameSetup;
