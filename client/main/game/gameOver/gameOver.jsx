7e8ee1e8f8264964e1b97839d1edf4a250fbd401
Kellen Steffen <kellen.steffen@thalmic.com>
2017-03-12 00:57:24 -0500

    Add game over message.

--- client/main/game/gameOver/gameOver.jsx
+++ client/main/game/gameOver/gameOver.jsx
@@ -1,13 +1,21 @@
 'use strict';
 
 const React = require('react');
+const _ = require('lodash');
 
 
 const GameOver = React.createClass({
 
+	getDefaultProps: function() {
+		return {
+			game: null,
+		};
+	},
+
 	render: function() {
 		return (
 			<div className="gameOver">
+				{this.props.game.state}
 			</div>
 		);
 	},
